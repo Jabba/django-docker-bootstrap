6399a23d82dfd339753348e825acfc9b316cfe9f
Jasper <jturnhout@gmail.com>
2018-01-31 09:40:36 +0100

    Add password change templates

--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -40,5 +40,9 @@ urlpatterns += [
     path( 'password_reset/done/', auth_views.password_reset_done, { 'template_name' : 'account/password_reset_done.html' },     name='password_reset_done' ),
     re_path( 'reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
         auth_views.password_reset_confirm,                        { 'template_name' : 'account/password_reset_confirm.html' },  name='password_reset_confirm' ),
-    path( 'reset/done/', auth_views.password_reset_complete,      { 'template_name' : 'account/password_reset_complete.html' }, name='password_reset_complete' )
+    path( 'reset/done/', auth_views.password_reset_complete,      { 'template_name' : 'account/password_reset_complete.html' }, name='password_reset_complete' ),
+
+    path( 'password_change/', auth_views.password_change, { 'template_name' : 'gentelella/password_change.html' }, name='password_change' ),
+    path( 'password_change_done/', auth_views.password_change_done, { 'template_name' : 'gentelella/password_change_done.html' }, name='password_change_done' ),
+
 ]
